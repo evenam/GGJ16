58a3679340db005b55dc72e6567df10025f1a661
Thomas Culotta <thomasculotta94@gmail.com>
2016-01-29 20:13:20 -0600

    Added ball and player base behavior; only need input and asthetics

---
+++ BeerPongClient/Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehavior : MonoBehaviour
+{
+	private int points;
+	private bool bounced;
+
+	public GameObject[] players;
+	private PlayerController myPlayer;
+	private PlayerController theirPlayer;
+
+	// Use this for initialization
+	void Start ()
+	{
+		points = 0;
+		bounced = false;
+
+		/*
+		 * Find active player.
+		 */
+		if (players [0].GetComponent<PlayerController> ().IsEnabled ())
+		{
+			myPlayer = players [0].GetComponent<PlayerController> ();
+			theirPlayer = players [1].GetComponent<PlayerController> ();
+		}
+		else
+		{
+			myPlayer = players [1].GetComponent<PlayerController> ();
+			theirPlayer = players[0].GetComponent<PlayerController> ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		/*
+		 * When player runs out of time.
+		 */
+		if (!myPlayer.IsEnabled ()) {
+			points = 0;
+			EndTurn ();
+		}
+	}
+
+	/*
+	 * Handles ball outcomes.
+	 * Landing in cup.
+	 * Bouncing on the table.
+	 * Landing between three cups.
+	 * Landing in an island cup.
+	 * Bouncing back to player's side.
+	 */
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.name.Contains ("Cup")) {
+			if (bounced)
+				points = 2;
+			else
+				points = 1;
+			EndTurn ();
+		} else if (other.name == "Table") {
+			bounced = true;
+		} else if (other.name.Contains ("TriCup")) {
+			points = 5;
+			EndTurn ();
+		} 
+		else if (other.name.Contains("Island"))
+		{
+			points = 3;
+			EndTurn ();
+		}
+		else if (other.name.Contains ("ReturnTrigger"))
+		{
+			ResetTurn ();
+		}
+	}
+
+	/*
+	 * Reset when ball returns to player's side.
+	 */
+	void ResetTurn()
+	{
+		myPlayer.StartTimer ();
+		Destroy (gameObject);
+	}
+
+	/*
+	 * Allocate current points to player.
+	 * Transfer active status to other player.
+	 * Kill me.
+	 */
+	void EndTurn()
+	{
+		myPlayer.ReceivePoints (points);
+		myPlayer.DisablePlay ();
+		theirPlayer.EnablePlay ();
+		Destroy (gameObject);
+	}
+}
---
+++ BeerPongClient/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	private bool playEnabled;
+	private float timeStart;
+	private int myPoints;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playEnabled = false;
+		myPoints = 0;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Time.time - timeStart > 30) {
+			TimeUp ();
+		}
+	}
+
+	/*
+	 * Receive points from current ball.
+	 */
+	public void ReceivePoints(int points)
+	{
+		myPoints += points;
+	}
+
+
+	/*
+	 * Only one player is active at a time.
+	 * Start timer on activate.
+	 */
+	public bool IsEnabled()
+	{
+		return playEnabled;
+	}
+
+	public void EnablePlay()
+	{
+		playEnabled = true;
+		StartTimer ();
+	}
+
+	public void DisablePlay()
+	{
+		playEnabled = false;
+	}
+
+
+	/*
+	 * Timer for player's turn.
+	 * 30 second timer.
+	 * Started on activate.
+	 * Can be reset if ball bounces back to player's own side when thrown.
+	 */
+	public void StartTimer()
+	{
+		timeStart = Time.time;
+	}
+
+	void TimeUp()
+	{
+		DisablePlay ();
+	}
+}
