ea8d17094b0248ed64ff9dbb9cdc8b00c72f130d
Thomas Culotta <sculot3@lsu.edu>
2016-01-31 11:32:22 -0600

    GUI auto-select user text, tab to switch, enter to enter

---
+++ BeerPongClient/Assets/Scripts/GUIHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class GUIHandler : MonoBehaviour {
+
+    public InputField userField;
+    public InputField opfoField;
+    public Button connectButton;
+
+	// Use this for initialization
+	void Start ()
+    {
+        userField.ActivateInputField();
+        opfoField.ActivateInputField();
+        userField.Select();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyUp(KeyCode.Tab))
+            SwitchField();
+        if (Input.GetKeyDown(KeyCode.Return))
+            ConnectToServer();
+	}
+
+    void SwitchField()
+    {
+        if (userField.isFocused)
+            opfoField.Select();
+        else
+            userField.Select();
+    }
+
+    void ConnectToServer()
+    {
+        connectButton.onClick.Invoke();
+        this.enabled = false;
+    }
+}
