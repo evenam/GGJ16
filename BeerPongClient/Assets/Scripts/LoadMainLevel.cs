90e0b6fd8c93a7a7715b1dab8a8de28f0438e319
Thomas Culotta <sculot3@lsu.edu>
2016-01-31 13:23:54 -0600

    Added intro scene shit

---
+++ BeerPongClient/Assets/Scripts/LoadMainLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMainLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+	}
+}
